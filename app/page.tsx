b10286b921248c68673227d048b09440b2eaaa9a
Deri Kurniawan <deri.netuchi@gmail.com>
2023-03-07 17:40:26 +0700

    feat: add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,56 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Image from "next/image";
+import Navbar from "@/components/Navbar";
+import guavaImg from "../assets/guava.jpg";
+import Link from "next/link";
+import SafeLayout from "@/layouts/SafeLayout";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <SafeLayout>
+      <Navbar />
+      <main className="px-[20px] lg:px-[119px] my-[16px] md:my-[48px]">
+        <div className="md:grid grid-flow-row grid-cols-12 gap-[32px] items-center">
+          {/* hero left */}
+          <div className="col-span-12 lg:col-span-7 lg:max-w-[600px]">
+            <h2 className="text-[30px] md:text-[40px] font-bold leading-[38px] md:leading-[48px] mb-2 md:mb-4">
+              Diagnosa Hama dan Penyakit Pada Tanaman Jambu Kristal Anda
+              Sekarang
+            </h2>
+            <p className="font-normal text-sm md:text-lg mb-3 md:mb-7">
+              Sistem berbasis web ini dapat membantu anda dalam mendiagnosa hama
+              dan penyakit pada tanaman jambu kristal anda, serta dapat
+              memberikan solusi atas masalah yang dialami oleh tanaman jambu
+              kristal anda secara gratis.
+            </p>
+
+            <div className="flex flex-1 flex-wrap gap-4">
+              <Link
+                className="btn btn-outline btn-info text-sm md:text-base capitalize"
+                href="/consult"
+              >
+                Konsultasi Sekarang
+              </Link>
+              <Link
+                className="btn btn-outline btn-success text-sm md:text-base capitalize"
+                href="/about"
+              >
+                Tentang Aplikasi
+              </Link>
+            </div>
+          </div>
+          {/* hero right */}
+          <div className="hidden lg:flex justify-end lg:col-span-5">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
+              className="w-[481px] h-[410px] rounded-2xl object-cover"
+              src={guavaImg}
+              alt=""
               priority
             />
-          </a>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </main>
+      <Footer />
+    </SafeLayout>
+  );
 }
