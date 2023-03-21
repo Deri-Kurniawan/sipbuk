0cd8bd8611111efe92103165e11f0173857064bb
Deri Kurniawan <deri.netuchi@gmail.com>
2023-03-22 02:01:01 +0700

    feat: add page loading indicator add: nextjs-progressbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from "next/app";
 import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
+import NextNProgressbar from "nextjs-progressbar";
 import "@/styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -8,6 +9,13 @@ const inter = Inter({ subsets: ["latin"] });
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <NextNProgressbar
+        color="#00D87F"
+        height={5}
+        showOnShallow={true}
+        options={{ easing: "ease", speed: 500 }}
+      />
+
       <Toaster
         position="bottom-left"
         reverseOrder={false}
@@ -19,6 +27,7 @@ export default function App({ Component, pageProps }: AppProps) {
           },
         }}
       />
+
       <div className={inter.className}>
         <Component className={inter.className} {...pageProps} />
       </div>
