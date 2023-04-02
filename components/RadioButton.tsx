f3484245ed1487d7916f097f35839560fc7e86f3
Deri Kurniawan <deri.netuchi@gmail.com>
2023-04-03 00:40:37 +0700

    feat: update radio button as regular button

--- components/RadioButton.tsx
+++ components/RadioButton.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { uncertaintyValueRawData } from "@/prisma/data";
 import { useState } from "react";
 
@@ -25,16 +23,32 @@ export default function RadioButton({ sympCode }: Props) {
     setSelectedOption(option);
   };
 
+  const handleButtonClick = (option: string) => {
+    handleOptionChange(option);
+  };
+
+  const handleButtonKeyDown = (event: React.KeyboardEvent<HTMLButtonElement>, option: string) => {
+    if (event.key === " " || event.key === "Enter") {
+      event.preventDefault();
+      handleButtonClick(option);
+    }
+  };
+
   return (
     <div className="grid grid-flow-row grid-cols-3 gap-4">
       {options.map((option: Option, index: number) => (
-        <label
+        <button
           key={index}
           tabIndex={0}
           className={`${selectedOption === option.label
-            ? "bg-blue-500 text-white"
-            : "bg-primary"
-            } col-span-1 text-sm text-center inline-flex justify-center lg:h-[42px] items-center px-4 py-2 rounded-lg transition-all duration-300 hover:cursor-pointer`}
+            ? "btn-primary"
+            : "btn-outline btn-primary"
+            } btn col-span-1 text-xs text-center rounded-lg p-2`}
+          aria-pressed={selectedOption === option.label}
+          role="button"
+          onKeyDown={(event) => handleButtonKeyDown(event, option.label)}
+          onClick={() => handleButtonClick(option.label)}
+          type="button"
         >
           <input
             type="radio"
@@ -45,7 +59,7 @@ export default function RadioButton({ sympCode }: Props) {
             onChange={() => handleOptionChange(option.label)}
           />
           {option.label}
-        </label>
+        </button>
       ))}
     </div>
   );
