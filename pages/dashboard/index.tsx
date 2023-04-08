27f276d6fe7599c70260f7fc0aee76073be3dc6f
Deri Kurniawan <deri.netuchi@gmail.com>
2023-04-08 21:52:03 +0700

    feat: add user dashboard content

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { hasCookie, getCookie, deleteCookie } from "cookies-next";
 import { PrismaClient } from '@prisma/client';
 import { serverSideAESDecrypt } from '@/utils/cryptoAES';
 import Navbar from '@/components/Navbar';
+import Head from 'next/head';
+import SafeLayout from '@/layouts/SafeLayout';
+import Link from 'next/link';
+import { toast } from 'react-hot-toast';
 
 export async function getServerSideProps({ req, res }: { req: any, res: any }) {
     const prisma = new PrismaClient();
@@ -22,9 +26,17 @@ export async function getServerSideProps({ req, res }: { req: any, res: any }) {
         const userCookie = JSON.parse(getCookie("user", { req, res }));
         const userFromDB = await prisma.user.findUnique({
             where: {
-                email: userCookie.email
+                email: userCookie.email,
             }
         });
+        const _userDiagnosesHistory = await prisma.usersDiagnoseHistory.findMany({
+            where: {
+                userId: userFromDB?.id,
+            },
+            include: {
+                pestsAndDeseases: true,
+            }
+        })
 
         if (!userFromDB) {
             deleteCookie("user", { req, res });
@@ -49,7 +61,8 @@ export async function getServerSideProps({ req, res }: { req: any, res: any }) {
         return {
             props: {
                 user: userCookie,
-            }
+                _userDiagnosesHistory: JSON.parse(JSON.stringify(_userDiagnosesHistory)),
+            },
         }
     } catch (error) {
         console.log(error)
@@ -65,13 +78,215 @@ export async function getServerSideProps({ req, res }: { req: any, res: any }) {
 
 interface DashboardProps {
     user: any;
+    _userDiagnosesHistory: {
+        id: string;
+        userId: string;
+        pestsAndDeseasesId: string;
+        finalCF: number;
+        userInputData: string; //json
+        createdAt: Date;
+        updatedAt: Date;
+        pestsAndDeseases: {
+            code: number;
+            name: string;
+            imageUrl: string;
+            createdAt: Date;
+            updatedAt: Date;
+            solution: string;
+        }
+    }[];
 }
 
-export default function Dashboard({ user }: DashboardProps) {
+const dayTimeText = () => {
+    const newDate = new Date();
+    const time = newDate.getHours();
+    if (time >= 0 && time < 12) {
+        return "Pagi";
+    }
+    if (time >= 12 && time < 15) {
+        return "Siang";
+    }
+    if (time >= 15 && time < 18) {
+        return "Sore";
+    }
+    if (time >= 18 && time < 24) {
+        return "Malam";
+    }
+}
+
+export default function Dashboard({ user, _userDiagnosesHistory }: DashboardProps) {
+    const [userDiagnosesHistory, setUserDiagnosesHistory] = useState(_userDiagnosesHistory);
+    const [localStorageDiagnosesHistory, setLocalStorageDiagnosesHistory] = useState([]);
+    const [selectedDiagnosesHistoryId, setSelectedDiagnosesHistoryId]: string[] | any = useState([]);
+    const globalCheckboxRef = React.useRef<HTMLInputElement>(null);
+
+    const handleClickAccept = async () => {
+        try {
+            const savedLocalStorageDiagnosesHistory = (async () => await fetch("/api/dashboard/diagnoses-history", {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                method: "POST",
+                body: JSON.stringify({
+                    diagnosesId: localStorageDiagnosesHistory,
+                    userId: user.id,
+                }),
+            }))();
+
+            const res = await savedLocalStorageDiagnosesHistory;
+            const savedData = await res.json();
+
+            if (savedData.code === 200) {
+                setUserDiagnosesHistory([...userDiagnosesHistory, ...savedData.data]);
+
+                toast.promise(savedLocalStorageDiagnosesHistory, {
+                    loading: "Menyimpan riwayat diagnosa...",
+                    success: "Riwayat diagnosa berhasil disimpan",
+                    error: "Riwayat diagnosa gagal disimpan",
+                });
+            } else {
+                toast.error(savedData.message);
+            }
+
+            if (typeof window !== "undefined") {
+                localStorage.removeItem("diagnosesHistoryId");
+            }
+
+            setLocalStorageDiagnosesHistory([]);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const handleClickRefuse = () => {
+        if (typeof window !== "undefined") {
+            localStorage.removeItem("diagnosesHistoryId");
+            setLocalStorageDiagnosesHistory([]);
+        }
+        setLocalStorageDiagnosesHistory([]);
+    }
+
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            setLocalStorageDiagnosesHistory(JSON.parse(localStorage.getItem("diagnosesHistoryId") || "[]"));
+        }
+    }, [])
+
     return (
         <>
+            <Head>
+                <title>Dashboard - SIPBUK</title>
+                <meta name="description" content="Sistem Pakar berbasis web ini dapat membantu anda dalam mendiagnosa hama dan penyakit pada tanaman jambu kristal anda, serta dapat memberikan solusi atas masalah yang dialami oleh tanaman jambu kristal anda secara gratis." />
+            </Head>
             <Navbar user={user} />
-            <h1>Dashboard</h1>
+            <SafeLayout>
+                <main className="safe-horizontal-padding my-[16px] md:my-[48px]">
+                    <h4 className="mb-3 text-xl font-bold">
+                        Selamat {dayTimeText()} 👋 {user.fullname || "Tanpa nama"}
+                    </h4>
+                    <div className='transition-all duration-500 ease-in-out'>
+                        {localStorageDiagnosesHistory.length > 0 && (
+                            <div className={`my-6 shadow-lg alert`}>
+                                <div>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="flex-shrink-0 w-6 h-6 stroke-info"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                    <span>{`Kamu punya ${localStorageDiagnosesHistory.length} riwayat diagnosa sebelumnya, apakah ingin anda simpan?`}</span>
+                                </div>
+                                <div className="flex-none">
+                                    <button className={`btn btn-sm btn-ghost`} onClick={handleClickRefuse}>Tolak</button>
+                                    <button className={`btn btn-sm btn-primary`} onClick={handleClickAccept}>Terima</button>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                    <div className="w-full mt-8 overflow-x-auto">
+                        <table className="table w-full">
+                            {/* head */}
+                            <thead>
+                                <tr>
+                                    <th>
+                                        <label>
+                                            <input type="checkbox" className="checkbox" ref={globalCheckboxRef} onChange={() => {
+                                                if (globalCheckboxRef.current?.checked) {
+                                                    setSelectedDiagnosesHistoryId(userDiagnosesHistory.map((diagnosesHistory: any) => diagnosesHistory.id));
+                                                } else {
+                                                    setSelectedDiagnosesHistoryId([]);
+                                                }
+                                            }} />
+                                        </label>
+                                    </th>
+                                    <th>Terdiagnosa</th>
+                                    <th>Persentase</th>
+                                    <th>Tanggal Konsultasi</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {/* row 1 */}
+                                {userDiagnosesHistory.length > 0 ? (
+                                    <>
+                                        {userDiagnosesHistory.map((diagnosesHistory: {
+                                            id: string;
+                                            userId: string;
+                                            pestsAndDeseasesId: string;
+                                            finalCF: number;
+                                            userInputData: string; //json
+                                            createdAt: Date;
+                                            updatedAt: Date;
+                                            pestsAndDeseases: {
+                                                code: number;
+                                                name: string;
+                                                imageUrl: string;
+                                                createdAt: Date;
+                                                updatedAt: Date;
+                                                solution: string;
+                                            }
+                                        }, index: number) => (
+                                            <tr key={index.toString()}>
+                                                <th>
+                                                    <label>
+                                                        <input type="checkbox" className="checkbox" value={diagnosesHistory.id} checked={
+                                                            selectedDiagnosesHistoryId.includes(diagnosesHistory.id)
+                                                        } onChange={() => {
+                                                            if (selectedDiagnosesHistoryId.includes(diagnosesHistory.id)) {
+                                                                setSelectedDiagnosesHistoryId(selectedDiagnosesHistoryId.filter((selectedDiagnosesHistoryId: string) => selectedDiagnosesHistoryId !== diagnosesHistory.id));
+                                                            } else {
+                                                                setSelectedDiagnosesHistoryId([...selectedDiagnosesHistoryId, diagnosesHistory.id]);
+                                                            }
+                                                        }} />
+                                                    </label>
+                                                </th>
+                                                <td>
+                                                    {diagnosesHistory.pestsAndDeseases.name}
+                                                </td>
+                                                <td>
+                                                    {diagnosesHistory.finalCF % 2 === 0 ? (diagnosesHistory.finalCF * 100).toFixed(0) : (diagnosesHistory.finalCF * 100).toFixed(2)}%
+                                                </td>
+                                                <td>{new Date(diagnosesHistory.createdAt).toLocaleString("id-ID", {
+                                                    year: "numeric",
+                                                    month: "long",
+                                                    day: "numeric",
+                                                    hour: "numeric",
+                                                    minute: "numeric",
+                                                    second: "numeric",
+                                                })}</td>
+                                                <th>
+                                                    <Link href={`/consult/${diagnosesHistory.id}`} className="btn btn-ghost btn-xs">Lihat Detail</Link>
+                                                </th>
+                                            </tr>
+                                        ))}
+                                    </>
+                                ) : (
+                                    <tr>
+                                        <td colSpan={5} className="text-center">
+                                            <div className="text-gray-500">Tidak ada riwayat diagnosa</div>
+                                        </td>
+                                    </tr>
+                                )}
+                            </tbody>
+                        </table>
+                    </div>
+                </main>
+            </SafeLayout>
         </>
     )
 }
