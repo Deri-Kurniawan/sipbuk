b16938ca6bf2eb0f8c4626158431e4050552e67a
Deri Kurniawan <deri.netuchi@gmail.com>
2023-04-12 08:51:16 +0700

    feat: add default seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,26 @@ import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import NextNProgressbar from "nextjs-progressbar";
 import "@/styles/globals.css";
+import { DefaultSeo } from "next-seo";
+import { useRouter } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   return (
     <>
+      <DefaultSeo
+        description="Sistem Pakar berbasis web ini dapat membantu anda dalam mendiagnosa hama dan penyakit pada tanaman jambu kristal anda, serta dapat memberikan solusi atas masalah yang dialami oleh tanaman jambu kristal anda secara gratis."
+        openGraph={{
+          type: 'website',
+          locale: 'id_ID',
+          url: process.env.APP_URL + router.asPath,
+          siteName: process.env.APP_NAME,
+        }}
+      />
+
       <NextNProgressbar
         color="#00D87F"
         height={5}
