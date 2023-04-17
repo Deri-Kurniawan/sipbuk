8b5e61f8eb32202b4bfa6f4766af8e3e2a5eea60
Deri Kurniawan <deri.netuchi@gmail.com>
2023-04-17 19:41:00 +0700

    feat: add keybindings, keyboard shortcut info

--- pages/consult/index.tsx
+++ pages/consult/index.tsx
@@ -1,7 +1,7 @@
 import Navbar from "@/components/Navbar";
 import Question from "@/components/Question";
 import { GrPrevious, GrNext } from "react-icons/gr";
-import { FormEventHandler, Fragment, useEffect, useState } from "react";
+import { FormEventHandler, Fragment, useEffect, useRef, useState } from "react";
 import Head from "next/head";
 import { getCookie, hasCookie } from "cookies-next";
 import { PrismaClient } from "@prisma/client";
@@ -9,6 +9,7 @@ import Link from "next/link";
 import { toast } from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { NextApiRequest, NextApiResponse } from "next";
+import { AiOutlineQuestionCircle } from "react-icons/ai";
 
 type getServerSidePropsType = {
   req: NextApiRequest;
@@ -75,6 +76,7 @@ export default function Consult({ user, questionList }: ConsultProps) {
     id: "question-0",
     index: 0,
   });
+  const formRef = useRef<HTMLFormElement>(null);
   const router = useRouter();
 
   const handleFormSubmit: FormEventHandler<HTMLFormElement> = async (e: any) => {
@@ -265,6 +267,20 @@ export default function Consult({ user, questionList }: ConsultProps) {
     });
   }, []);
 
+  useEffect(() => {
+    const handleCtrlEnter = (e: KeyboardEvent) => {
+      if (e.ctrlKey && e.key === "Enter" && !fetchIsLoading) {
+        formRef && formRef.current && formRef.current.requestSubmit();
+      }
+    };
+
+    document.addEventListener("keydown", handleCtrlEnter);
+
+    return () => {
+      document.removeEventListener("keydown", handleCtrlEnter);
+    };
+  }, [fetchIsLoading])
+
   return (
     <>
       <Head>
@@ -272,11 +288,25 @@ export default function Consult({ user, questionList }: ConsultProps) {
         <meta name="description" content="Sistem Pakar berbasis web ini dapat membantu anda dalam mendiagnosa hama dan penyakit pada tanaman jambu kristal anda, serta dapat memberikan solusi atas masalah yang dialami oleh tanaman jambu kristal anda secara gratis." />
       </Head>
       <Navbar isSticky={false} user={user} />
-      <main className="safe-horizontal-padding my-[16px] md:my-[48px]">
+      <main className="safe-horizontal-padding mt-[16px] md:mt-[48px]">
         {questionList && questionList?.length > 0 ? (
           <Fragment>
+            <div className="flex flex-col items-center justify-center text-center my-[112px] lg:my-[172px]">
+              <h4 className="mb-3 text-3xl font-bold max-w-[552px]">
+                Lingkungan Konsultasi
+              </h4>
+              <p className="mb-6 text-base max-w-[552px]">
+                Dalam lingkungan konsultasi, anda akan diberikan beberapa pertanyaan pilihan, yang harus anda pilih sesuai dengan yang anda ketahui atau anda yakini.
+              </p>
+              <a href="#question-start"
+                className={`capitalize btn btn-active btn-ghost`}
+                type="submit"
+              >
+                Memulai
+              </a>
+            </div>
             {/* questions */}
-            <form onSubmit={handleFormSubmit}>
+            <form ref={formRef} onSubmit={handleFormSubmit} id="question-start">
               {questionList.map((ql: any, index: number) => (
                 <div
                   key={index}
@@ -351,6 +381,72 @@ export default function Consult({ user, questionList }: ConsultProps) {
           </button>
         </div>
       </div>
+
+      {/* floating shortcut help */}
+      <label htmlFor="kbd-modal" className="fixed right-0 hidden lg:block hover:cursor-help bottom-1/4" title="Pintasan Papan Ketik">
+        <div className="flex items-center justify-center w-12 h-10 rounded-tl-lg rounded-bl-lg shadow-lg bg-primary">
+          <AiOutlineQuestionCircle className="text-3xl" />
+        </div>
+      </label>
+      {/* end of floating shortcut help */}
+
+      {/* floating shorcut help modal */}
+      <input type="checkbox" id="kbd-modal" className="modal-toggle" />
+      <div className="modal">
+        <div className="w-11/12 max-w-3xl modal-box">
+          <h3 className="text-4xl font-bold text-center">Pintasan Papan Ketik</h3>
+          <div className="py-6">
+            <div className="w-full overflow-x-auto">
+              <table className="table w-full">
+                <thead>
+                  <tr>
+                    <th>Pengikat Kunci</th>
+                    <th>Fungsi</th>
+                    <th>Coba Fungsi</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      <kbd className="kbd">Ctrl</kbd>
+                      <span className="px-2">+</span>
+                      <kbd className="kbd">▶︎</kbd>
+                    </td>
+                    <td>Pertanyaan Selanjutnya</td>
+                    <td><button className="btn" onClick={handleClickNextQuestion}>Coba</button></td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <kbd className="kbd">Ctrl</kbd>
+                      <span className="px-2">+</span>
+                      <kbd className="kbd">◀︎</kbd>
+                    </td>
+                    <td>Pertanyaan Sebelumnya</td>
+                    <td><button className="btn" onClick={handleClickPrevQuestion}>Coba</button></td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <kbd className="kbd">Ctrl</kbd>
+                      <span className="px-2">+</span>
+                      <kbd className="kbd">Enter</kbd>
+                    </td>
+                    <td>Selesai dan Diagnosa</td>
+                    <td><button className="btn" onClick={() => {
+                      if (!fetchIsLoading) {
+                        formRef && formRef.current && formRef.current.requestSubmit();
+                      }
+                    }}>Coba</button></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div className="modal-action">
+            <label htmlFor="kbd-modal" className="btn">Tutup</label>
+          </div>
+        </div>
+      </div>
+      {/* end of floating shorcut help modal */}
     </>
   );
 }
