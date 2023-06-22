4ac27504a4d4457d8eeed9f82cfed6a8d2d54e21
Deri Kurniawan <deri.netuchi@gmail.com>
2023-06-22 17:48:23 +0700

    feat: update jest test

--- __test__/inferenceEngine.spec.js
+++ __test__/inferenceEngine.spec.js
@@ -1,116 +1,74 @@
 import CertaintyFactor from "../utils/certaintyFactor";
-import { userInputTemplate } from "../prisma/data";
 
-test("pengujian ketersediaan dan kesesuaian aturan basis pengetahuan", async () => {
-  const pestsDeseaseEachRelationLength = [5, 4, 6, 2, 6, 3, 4, 5];
-  const instance = new CertaintyFactor({});
-  const data = await (await instance.generateKnowledgeBase()).knowledgeBase;
+test("knowledge base harus memiliki 8 hama dan penyakit serta keterikatan terhadap gejalanya", async () => {
+  const expectedPestDeseaseDataLength = 8;
+  const expectedPestsDeseaseRelationRuleLength = {
+    HP1: 5,
+    HP2: 4,
+    HP3: 6,
+    HP4: 2,
+    HP5: 6,
+    HP6: 3,
+    HP7: 4,
+    HP8: 5,
+  };
+  const userInput = {};
 
-  expect(data).not.toBeNull();
-  expect(data).toHaveLength(8);
+  const instance = new CertaintyFactor(userInput);
+  const { knowledgeBase } = await instance.generateKnowledgeBase();
 
-  pestsDeseaseEachRelationLength.forEach((length, index) => {
-    expect(data[index].PestsAndDeseasesHasSymptoms).toHaveLength(length);
+  expect(knowledgeBase).not.toBeNull();
+  expect(knowledgeBase).toBeInstanceOf(Array);
+  expect(knowledgeBase).toHaveLength(expectedPestDeseaseDataLength);
+  knowledgeBase.forEach((item, index) => {
+    expect(item).toHaveProperty("PestsAndDeseasesHasSymptoms");
+    expect(item.PestsAndDeseasesHasSymptoms).toHaveLength(
+      expectedPestsDeseaseRelationRuleLength[`HP${++index}`]
+    );
   });
 });
 
-test("pengujian penggabungan basis pengetahuan dengan nilai masukan pengguna", async () => {
-  const userInput = {
-    ...userInputTemplate,
-    13: 0.4,
-    19: 0.6,
-    20: 0.8,
-  };
-
-  const instance = new CertaintyFactor(userInput);
-  const data = await (
-    await instance.mixKnowledgeBaseWithUserCFInput()
-  ).knowledgeBase;
-
-  expect(data).not.toBeNull();
-  expect(data).toHaveLength(8);
-
-  data.forEach((item) => {
-    item.PestsAndDeseasesHasSymptoms.forEach((item) => {
-      expect(item).toHaveProperty("userCF");
-    });
-  });
-});
-
-test("pengujian penggunaan rumus aturan tunggal certainty factor", async () => {
-  const userInput = {
-    ...userInputTemplate,
-    13: 0.4,
-    19: 0.6,
-    20: 0.8,
-  };
-
-  const instance = new CertaintyFactor(userInput);
-  const data = (await instance.calculateSingleRuleCF()).calculatedSingleRuleCF;
-
-  expect(data).not.toBeNull();
-  expect(data[0]).toHaveProperty("calculatedSingleRuleCF");
-});
-
-test("pengujian penggunaan rumus aturan kombinasi certainty factor", async () => {
-  const userInput = {
-    ...userInputTemplate,
-    13: 0.4,
-    19: 0.6,
-    20: 0.8,
-  };
-
-  const instance = new CertaintyFactor(userInput);
-  const data = (await instance.calculateCombinationRule())
-    .calculatedCombinationRuleCF;
-
-  expect(data).not.toBeNull();
-  expect(data[0]).toHaveProperty("calculatedCombinationRuleCF");
-});
-
-test("pengujian hasi diagnosa certainty factor adalah Penyakit Embun Jelaga dengan nilai 0.9456", async () => {
-  const expectation = {
-    name: "Penyakit Embun Jelaga",
-    finalCF: 0.9456,
-  };
-
-  const userInput = {
-    ...userInputTemplate,
-    13: 0.4,
-    19: 0.6,
-    20: 0.8,
-  };
-
-  const instance = new CertaintyFactor(userInput);
-  const data = (await instance.generateConclusion()).conclusion;
-
-  expect(data).not.toBeNull();
-  expect(data).toHaveProperty("name");
-  expect(data.name).toBe(expectation.name);
-  expect(data).toHaveProperty("finalCF");
-  expect(data.finalCF).toBe(expectation.finalCF);
-});
-
-test("pengujian hasi diagnosa certainty factor adalah Hama Lalat Buah dengan nilai 0.984", async () => {
+test("hasil diagnosa harus mengindikasikan Hama Lalat Buah dengan nilai 0.9840", async () => {
   const expectation = {
     name: "Hama Lalat Buah",
     finalCF: 0.984,
-    percetagge: 98.4,
   };
-
   const userInput = {
-    ...userInputTemplate,
     1: 0.8,
     2: 0.8,
+    3: 0,
     4: 0.6,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+    9: 0,
+    10: 0,
+    11: 0,
+    12: 0,
+    13: 0,
+    14: 0,
+    15: 0,
+    16: 0,
+    17: 0,
+    18: 0,
+    19: 0,
+    20: 0,
+    21: 0,
+    22: 0,
+    23: 0,
+    24: 0,
+    25: 0,
+    26: 0,
+    27: 0,
+    28: 0,
+    29: 0,
   };
 
   const instance = new CertaintyFactor(userInput);
-  const data = (await instance.generateConclusion()).conclusion;
+  const { conclusion } = await instance.generateConclusion();
 
-  expect(data).not.toBeNull();
-  expect(data).toHaveProperty("name");
-  expect(data.name).toBe(expectation.name);
-  expect(data).toHaveProperty("finalCF");
-  expect(data.finalCF.toFixed(3)).toBe(expectation.finalCF.toFixed(3));
+  expect(conclusion).not.toBeNull();
+  expect(conclusion.name).toBe(expectation.name);
+  expect(conclusion.finalCF.toFixed(4)).toBe(expectation.finalCF.toFixed(4));
 });
