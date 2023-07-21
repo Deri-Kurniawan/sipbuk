dff87fe55e52d232a5fd411ef9ad6e13d5ec0696
Deri Kurniawan <deri.netuchi@gmail.com>
2023-07-22 02:05:28 +0700

    feat: update symptom informations

--- pages/admin/pests-deseases/set-rule/[code].tsx
+++ pages/admin/pests-deseases/set-rule/[code].tsx
@@ -188,7 +188,7 @@ const Admin = ({ user, pestsOrDeseases, symptoms }: AdminProps) => {
                         <li>
                             <a>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-4 h-4 mr-2 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path></svg>
-                                Pengaturan Rule untuk {pestsOrDeseases.name}
+                                Pengaturan Rule untuk [HP{pestsOrDeseases.code}] {pestsOrDeseases.name}
                             </a>
                         </li>
                     </ul>
@@ -196,15 +196,15 @@ const Admin = ({ user, pestsOrDeseases, symptoms }: AdminProps) => {
                 <form onSubmit={onSubmitHandler} ref={formRef}>
                     <div className="flex items-center justify-between">
                         <h4 className="mb-2 text-xl font-bold">
-                            Pengaturan Rule untuk ({pestsOrDeseases.name})
+                            Pengaturan Rule untuk [HP{pestsOrDeseases.code}] {pestsOrDeseases.name}
                         </h4>
                         <button className={`btn btn-primary ${fetchIsLoading ? 'loading' : ''}`} type='submit' disabled={fetchIsLoading}>Simpan Rule</button>
                     </div>
                     <div className="mt-4">
                         <div className="mb-4 mockup-code">
                             <pre data-prefix=">">
-                                <code>Aturan dipilih: </code>
-                                <code>{JSON.stringify(selectedSymptomData)}</code>
+                                <code>Aturan gejala dipilih: </code>
+                                <code>G{JSON.stringify(selectedSymptomData)}</code>
                             </pre>
                         </div>
                         <div className="w-full overflow-x-auto">
